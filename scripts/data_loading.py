124d9c004d5a9a318095c1b73e4a46489609cda6
Tyffoni <tyffoni@gmail.com>
2026-06-26 18:16:46 -0700

    import os to allow for search via absolute file path

--- scripts/data_loading.py
+++ scripts/data_loading.py
@@ -1,7 +1,8 @@
 import mysql.connector
 import pandas as pd
+import os
 conn = mysql.connector.connect(
-    host='localhost', user='root', password='root', database='real_estate')
+    host='localhost', user='root', password='root', database='idx_exchange')
 query = """
 SELECT L_ListingID, L_Address, L_City, L_Keyword2 as beds,
     LM_Dec_3 as baths, L_SystemPrice as price, L_Remarks as remarks
@@ -10,5 +11,8 @@ WHERE L_Remarks IS NOT NULL AND LENGTH(L_Remarks) > 50
 ORDER BY RAND() LIMIT 1000
 """
 df = pd.read_sql(query, conn)
-df.to_csv('data/processed/listing_sample.csv', index=False)
+# df.to_csv('data/processed/listing_sample.csv', index=False)
+df.to_csv(os.path.abspath('../data/processed/listing_sample.csv'), index=False)
+
+
 conn.close()
