b3b3c0f3c355faecf698a3f0c81965c7348a04c7
Tyffoni <tyffoni@gmail.com>
2026-07-03 13:47:37 -0700

    when running the script, delete "cleaned_remarks.txt" if it doesn't exists

--- scripts/cleaned_list_builder.py
+++ scripts/cleaned_list_builder.py
@@ -1,19 +1,26 @@
 from scripts.text_cleaning import TextCleaner
 import pandas as pd
+import os
 
 cleaner = TextCleaner()
 df = pd.read_csv('data/processed/listing_sample.csv')
+print("Original remarks Length:", len(df['remarks'].dropna()))
+
 
 # for remark in df['remarks'].dropna():
 #     cleaned_remark = cleaner.clean_text(remark)
 #     print(f"Original: {remark}\nCleaned: {cleaned_remark}\n")
-# for remark in df['remarks'].dropna():
-#     cleaned_remark = cleaner.clean_text(remark)
-#     with open('data/processed/cleaned_remarks.txt', 'a') as f:
-#         f.write(f"Original: {remark}\nCleaned: {cleaned_remark}\n\n")
+
+os.remove('data/processed/cleaned_remarks.txt') if os.path.exists('data/processed/cleaned_remarks.txt') else None
+for remark in df['remarks'].dropna():
+    cleaned_remark = cleaner.clean_text(remark)
+    with open('data/processed/cleaned_remarks.txt', 'a') as f:
+        f.write(f"Original: {remark}\nCleaned: {cleaned_remark}\n\n")
 
 cleaned_remarks = df['remarks'].dropna().apply(cleaner.clean_text)
 df.drop(columns=['remarks'], inplace=True)
 df['cleaned_remarks'] = cleaned_remarks
+print("Cleaned remarks Length:", len(df['cleaned_remarks'].dropna()))
+\
 df.to_csv('data/processed/cleaned_listing_sample.csv', index=False)
 
--- scripts/sample_builder.py
+++ scripts/sample_builder.py
@@ -6,3 +6,4 @@ randomInts = random.sample(range(len(df)), 100)
 sample_df = df.iloc[randomInts]
 print(len(sample_df))
 sample_df.to_csv('data/processed/test_suite.csv', index=False)
+
