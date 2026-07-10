a0fafa32834b2dd7a39485d6967c69700a7c50f5
Tyffoni <tyffoni@gmail.com>
2026-07-09 19:04:22 -0700

    reduce test_suite size

--- scripts/sample_builder.py
+++ scripts/sample_builder.py
@@ -2,7 +2,7 @@ import random
 import pandas as pd
 
 df = pd.read_csv('data/processed/listing_sample.csv')
-randomInts = random.sample(range(len(df)), 100)
+randomInts = random.sample(range(len(df)), 40)
 sample_df = df.iloc[randomInts]
 print(len(sample_df))
 sample_df.to_csv('data/processed/test_suite.csv', index=False)
