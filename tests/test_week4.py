85dbc186c3af0ccf1b8fe6eba872e648a81c9d51
Tyffoni <tyffoni@gmail.com>
2026-07-17 12:44:34 -0700

    query parser with regex

---
+++ tests/test_week4.py
@@ -0,0 +1,12 @@
+from scripts.query_parser import QueryParser
+from scripts.schema_validator import SchemaValidator
+
+parser = QueryParser()
+validator = SchemaValidator()
+filters = parser.parse("3 bed in Portland under 500k")
+valid, errors = validator.validate_query(filters)
+if not valid:
+    print(f"Query validation errors: {errors}")
+# Return helpful message to user
+else:
+    sql, params = parser.to_sql(filters)
