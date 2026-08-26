2631976290dca97e7f2574a77a67cb4370e2167b
Tyffoni <tyffoni@gmail.com>
2026-08-25 23:29:15 -0700

    add load taxamony method in SchemaValidator

--- scripts/schema_validator.py
+++ scripts/schema_validator.py
@@ -1,16 +1,21 @@
 import json
 import pandas as pd
-
+ 
 class SchemaValidator:
     # def __init__(self, schema_path='data/schema.json'):
     #     with open(schema_path) as f:
     #         self.schema = json.load(f)
     def __init__(self):
         self.valid_cities = self._load_valid_cities()
+        self.taxonomy = self._load_taxonomy()
     def _load_valid_cities(self):
         df = pd.read_csv('data/processed/listing_sample.csv')
         cities = set(df["L_City"].tolist())
         return cities
+    def _load_taxonomy(self):
+        with open('data/processed/taxonomy.json') as f:
+            taxonomy = json.load(f)
+        return taxonomy
     def validate_query(self, filters):
         errors = []
         # Check city exists in database
@@ -20,11 +25,16 @@ class SchemaValidator:
                 
         # Check price range
         if 'price_max' in filters:
-            if filters['price_max'] < 100000 or filters['price_max'] > 10000000:
+            if filters['price_max'] < 1000 or filters['price_max'] > 10000000:
                 errors.append(f"Price {filters['price_max']} outside typical range")
 
         # Check bedroom count
         if 'bedrooms' in filters:
             if filters['bedrooms'] < 1 or filters['bedrooms'] > 10:
                 errors.append(f"Bedroom count {filters['bedrooms']} seems invalid")
+
+        #check if the filters are in the taxonomy
+        # for key in filters:
+        #     if key not in self.taxonomy:
+        #         errors.append(f"Filter '{key}' is not recognized in taxonomy")
         return len(errors) == 0, errors
